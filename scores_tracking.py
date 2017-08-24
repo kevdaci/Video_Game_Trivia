74b578e8f6cece62453489eabd356779d82ad1a2
kevdaci <kevdaci@gmail.com>
2017-08-24 12:23:38 -0400

    Takes care of recording the scores to a Google spreadsheet

---
+++ scores_tracking.py
@@ -0,0 +1,34 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+class ScoresTracking(object):
+
+	scope = ["https://spreadsheets.google.com/feeds"]
+	creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
+	client = gspread.authorize(creds)
+	sheet = client.open("VideoGameTriviaApp").sheet1
+
+	def update_scores(self, name, score):
+		records = self.sheet.get_all_records()
+		index_new_score = self.__sort_cells(records, [score, name])
+		start_row = 2
+		if(index_new_score != -1):
+			self.sheet.insert_row([name, score], index_new_score + start_row)
+		self.sheet.resize(rows = 51)
+
+	def __sort_cells(self, records, new_entry):
+		pair_values = []
+		index = -1
+		for record in records:
+			pair_values.append([record["Score"], record["Name"]])
+		pair_values.append(new_entry)
+		pair_values.sort()
+		pair_values = pair_values [::-1]
+		try:
+			index = pair_values.index(new_entry)
+		except:
+			index = -1
+		return index
+
+	def get_scores_data(self):
+		return self.sheet.get_all_records()
